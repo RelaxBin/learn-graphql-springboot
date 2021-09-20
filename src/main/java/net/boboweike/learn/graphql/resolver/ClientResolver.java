9aa119c32a29043f04063f448e042edf6936a7ba
bobo <bobo@spring2go.com>
2021-09-20 17:32:03 +0800

    sec09 commit

--- src/main/java/net/boboweike/learn/graphql/resolver/ClientResolver.java
+++ src/main/java/net/boboweike/learn/graphql/resolver/ClientResolver.java
@@ -1,6 +1,7 @@
 package net.boboweike.learn.graphql.resolver;
 
-import graphql.GraphQLException;
+import graphql.execution.DataFetcherResult;
+import graphql.kickstart.execution.error.GenericGraphQLError;
 import graphql.kickstart.tools.GraphQLResolver;
 import lombok.extern.slf4j.Slf4j;
 import net.boboweike.learn.graphql.domain.BankAccount;
@@ -12,15 +13,16 @@ import java.util.UUID;
 @Slf4j
 @Component
 public class ClientResolver implements GraphQLResolver<BankAccount> {
-    public Client client(BankAccount bankAccount) {
+    public DataFetcherResult<Client> client(BankAccount bankAccount) {
         log.info("Retrieving client data for bank account {}", bankAccount.getId());
 
-        throw new GraphQLException("Spring exception, can't connect to DB: (sql select *)");
-
-//        return Client.builder()
-//                .id(UUID.randomUUID())
-//                .firstName("William")
-//                .lastName("Bobo1")
-//                .build();
+        return DataFetcherResult.<Client>newResult()
+                .data(Client.builder()
+                        .id(UUID.randomUUID())
+                        .firstName("William")
+                        .lastName("Bobo1")
+                        .build())
+                .error(new GenericGraphQLError("Can't get part of the client info"))
+                .build();
     }
 }
